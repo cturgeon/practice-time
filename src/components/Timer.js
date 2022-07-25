cf5ae867bace2256c8e7b24f3e7bbc81169279e3
cturgeon <casey.turgeon@gmail.com>
2022-07-24 22:05:05 -0500

    add: button

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,13 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import classes from "./Timer.module.css";
 
+// while click pause
+// just do minutes as editable
+
 export default function TimerComponent(props) {
-  const [timer, setTimer] = useState(0);
+  const [timer, setTimer] = useState("00:00:00");
+  const [seconds, setSeconds] = useState(0);
+  useEffect(() => {}, []);
+
+  const timerHandler = () => {
+    //
+  };
+
   return (
     <>
       <div className={classes.timer}>
         <h1>Timer Component</h1>
         <p>{timer}</p>
+        <button onClick={timerHandler}>Start</button>
       </div>
     </>
   );
