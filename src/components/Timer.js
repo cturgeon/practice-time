63fc3326253fbaba39a3d25b24e4916f9989fee6
cturgeon <casey.turgeon@gmail.com>
2022-07-26 00:12:58 -0500

    change: added setInterval

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -5,13 +5,28 @@ import classes from "./Timer.module.css";
 // just do minutes as editable
 
 export default function TimerComponent(props) {
-  const [timer, setTimer] = useState("00:00:00");
-  const [seconds, setSeconds] = useState(0);
-  useEffect(() => {}, []);
+  const [timer, setTimer] = useState("00:00");
+  const [countdownFlag, steCountdownFlag] = useState(false);
+  const [timerLenth, TimerLenth] = useState(0);
 
-  const timerHandler = () => {
-    //
-  };
+  // TEST: to see if interval can be used
+  // instead of setTimeout inside of useeffect
+  setInterval(() => {
+    // check if flagged to countdown
+    // if so: decriment timer by 1 second
+    if (!countdownFlag) {
+      // not sure if return works here
+      // I just want to not execute the following
+      return;
+    }
+    if (timerLenth > 0) {
+      // change setTimer to reflect current timer
+      if (timer[-2] === "0") {
+      }
+    }
+  }, 1000);
+
+  const timerHandler = () => {};
 
   return (
     <>
