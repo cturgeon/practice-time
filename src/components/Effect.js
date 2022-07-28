07534ae37947f73fe344c6fe3316cdc71af8a776
cturgeon <casey.turgeon@gmail.com>
2022-07-28 18:11:49 -0500

    test: trying to understand useEffect still...

---
+++ src/components/Effect.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+export default function EffectTestComponent(params) {
+  const [test, setTest] = useState(0);
+  const [bool, setBool] = useState(false);
+  useEffect(() => {
+    setTest(test + 1);
+  }, [bool]);
+  const buttonHandler = () => {
+    setBool(!bool);
+  };
+
+  return (
+    <div>
+      <h1>{test}</h1>
+      <button onClick={buttonHandler}>click</button>
+    </div>
+  );
+}
