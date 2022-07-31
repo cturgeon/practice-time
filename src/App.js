2b7be756f0fecca802e8cfebe7e320ffd394bfbe
cturgeon <casey.turgeon@gmail.com>
2022-07-30 23:08:06 -0500

    test: template from the web

--- src/App.js
+++ src/App.js
@@ -1,14 +1,81 @@
-import "./App.css";
-import TimerComponent from "./components/Timer";
+import React, { useState, useRef } from "react";
 
-function App() {
+const App = () => {
+  // We need ref in this, because we are dealing
+  // with JS setInterval to keep track of it and
+  // stop it when needed
+  const Ref = useRef(null);
+
+  // The state for our timer
+  const [timer, setTimer] = useState("00:00");
+  const inputNumber = useRef();
+
+  const getTimeRemaining = (e) => {
+    const total = Date.parse(e) - Date.parse(new Date());
+    console.log(total);
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    return {
+      total,
+      minutes,
+      seconds,
+    };
+  };
+
+  const startTimer = (e) => {
+    let { total, minutes, seconds } = getTimeRemaining(e);
+    if (total >= 0) {
+      // update the timer
+      // check if less than 10 then we need to
+      // add '0' at the beginning of the variable
+      setTimer(
+        (minutes > 9 ? minutes : "0" + minutes) +
+          ":" +
+          (seconds > 9 ? seconds : "0" + seconds)
+      );
+    }
+  };
+
+  const clearTimer = (e) => {
+    // If you adjust it you should also need to
+    // adjust the Endtime formula we are about
+    // to code next
+    setTimer("00:10");
+
+    // If you try to remove this line the
+    // updating of timer Variable will be
+    // after 1000ms or 1sec
+    if (Ref.current) clearInterval(Ref.current);
+    const id = setInterval(() => {
+      startTimer(e);
+    }, 1000);
+    Ref.current = id;
+  };
+
+  const getDeadTime = () => {
+    let deadline = new Date();
+
+    // This is where you need to adjust if
+    // you entend to add more time
+    deadline.setSeconds(deadline.getSeconds() + 10);
+    return deadline;
+  };
+
+  // Another way to call the clearTimer() to start
+  // the countdown is via action event from the
+  // button first we create function to be called
+  // by the button
+  const onClickReset = () => {
+    clearTimer(getDeadTime());
+  };
+
   return (
-    <>
-      <div>
-        <TimerComponent />
-      </div>
-    </>
+    <div className="App">
+      <input type="number" ref={inputNumber} />
+      <h2>{timer}</h2>
+      <button onClick={onClickReset}>Reset</button>
+    </div>
   );
-}
+};
 
 export default App;
